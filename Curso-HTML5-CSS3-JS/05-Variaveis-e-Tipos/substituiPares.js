90fc3be3c9ce219323b600abba466d8582f43d0d
LayanOliveira <layan3210@gmail.com>
2022-06-16 02:06:38 -0300

    Atualizado o script

--- Curso-HTML5-CSS3-JS/05-Variaveis-e-Tipos/substituiPares.js
+++ Curso-HTML5-CSS3-JS/05-Variaveis-e-Tipos/substituiPares.js
@@ -1,17 +1,17 @@
 let lista = [1, 3, 4, 6, 80, 33, 23, 90]
 
-function subistituiPares(array) {
+function substituiPares(array) {
     if (!array.length) return -1
-    for(let i = 0; i <array.length; i++) {
-        if (array[i] === 0){
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === 0) {
             console.log("Você já é zero!!")
-        } else if (array[i] % 2 === 0){
+        } else if (array[i] % 2 === 0) {
             console.log(`Substituindo ${array[i]} por 0...`)
             array[i] = 0
-        } 
+        }
     }
     return array
 }
 
 
-console.log(subistituiPares(lista))
+console.log(substituiPares(lista))
