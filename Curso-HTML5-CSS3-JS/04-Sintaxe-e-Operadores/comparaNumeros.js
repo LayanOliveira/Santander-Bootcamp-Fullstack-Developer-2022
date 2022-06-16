218050ad9b5399716ea5c1be6e0e999cc72ef37b
LayanOliveira <layan3210@gmail.com>
2022-06-15 21:47:59 -0300

    Adicionado o exercício comparador de números das aulas Sintaxe e Operadores

--- Curso-HTML5-CSS3-JS/04-Sintaxe-e-Operadores/comparaNumeros.js
+++ Curso-HTML5-CSS3-JS/04-Sintaxe-e-Operadores/comparaNumeros.js
@@ -1,20 +1,26 @@
 function comparaNumero(num1, num2) {
-    const iguais = saoMaior(num1, num2)
-    const asoma = soma(num1, num2)
-
-    return {saoIguais,soma}
+    const saoIguais = criaIguais(num1, num2)
+    const asoma = saoMaior(num1, num2)
+    if (num1 == null || num2 == null) { return "Favor entrar dois números" }
+    if (num1 != Number || num2 != Number) { return 'Favor entrar dois "NÚMEROS"' }
+    return `${saoIguais} ${asoma}`
 }
-function soma(num1, num2){
-    const soma = num1 + num2
-    const iguais = num1 !== num2
+function criaIguais(num1, num2) {
+    let iguais = `Os números ${num1} e ${num2}`
+    let siguais = 'não'
 
-    if (){}
-    
-}
-function saoMaior(num1, num2){
-
+    if (num1 === num2) { siguais = '' }
+    return `${iguais} ${siguais} são iguais.`
 }
 
+function saoMaior(num1, num2) {
+    dez = 'maior'
+    vinte = 'maior'
 
-console.log(comparaNumero(2, 2))
+    if (num1 + num2 < 10) { dez = 'menor' }
+    if (num1 + num2 < 20) { vinte = 'menor' }
+    return `Sua soma é ${num1 + num2}, que é ${dez} que 10 e ${vinte} que 20.`
+}
 
+
+console.log(comparaNumero(a))
