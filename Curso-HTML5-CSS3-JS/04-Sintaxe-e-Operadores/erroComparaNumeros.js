a72fb8d59476ffe6cc605fa6205d3ba86957485e
LayanOliveira <layan3210@gmail.com>
2022-06-20 02:15:21 -0300

    Tentando resolver erros do script comparaNumeros

---
+++ Curso-HTML5-CSS3-JS/04-Sintaxe-e-Operadores/erroComparaNumeros.js
@@ -0,0 +1,37 @@
+function comparaNumero(num1, num2) {
+    const saoIguais = criaIguais(num1, num2)
+    const asoma = saoMaior(num1, num2)
+    try {
+        if (num1 == null || num2 == null) { return "Entre dois números" }
+        if (typeof num1 !== 'number' && typeof !num2 !== 'number') throw new TypeError('Entre DOIS "NÚMEROS"')
+        if (!num1 || !num2) throw new ReferenceError("Parâmetro Inválido")
+        return `${saoIguais} ${asoma}`
+    } catch (e) {
+        if (e instanceof TypeError) {
+            console.log("Este erro é um TypeError")
+            console.log(e.message)
+        } else if (e instanceof ReferenceError) {
+            console.log("Este erro é um ReferenceError")
+            console.log(e.message)
+        }
+    }
+}
+function criaIguais(num1, num2) {
+    let iguais = `Os números ${num1} e ${num2}`
+    let siguais = 'não'
+
+    if (num1 === num2) { siguais = '' }
+    return `${iguais} ${siguais} são iguais.`
+}
+
+function saoMaior(num1, num2) {
+    dez = 'maior'
+    vinte = 'maior'
+
+    if (num1 + num2 < 10) { dez = 'menor' }
+    if (num1 + num2 < 20) { vinte = 'menor' }
+    return `Sua soma é ${num1 + num2}, que é ${dez} que 10 e ${vinte} que 20.`
+}
+
+
+console.log(comparaNumero(2, 1))
