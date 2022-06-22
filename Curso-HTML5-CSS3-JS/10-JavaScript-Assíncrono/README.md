9489187c24433addd35732fdbcb5c87f620613df
LayanOliveira <layan3210@gmail.com>
2022-06-21 23:58:09 -0300

    Adicionado Orientação a objetos JS e projeto html dogs a JS assíncrono

--- Curso-HTML5-CSS3-JS/10-JavaScript-Assíncrono/README.md
+++ Curso-HTML5-CSS3-JS/10-JavaScript-Assíncrono/README.md
@@ -6,12 +6,13 @@ Este repositório contém a atividade prática do Curso "JavaScript Assíncrono"
 
 Nesta atividade, vamos criar uma página que carrega fotos aleatórias de gatinhos sempre que clicamos em um botão.
 
-1. Utilize a API `https://thatcopy.pw/catapi/rest` para fazer as chamadas com o método `fetch()`;
+1. Utilize a API `https://thatcopy.pw/catapi/rest` para gatos e `https://dog.ceo/api/breeds/image/random` para cachorros, para fazer as chamadas com o método `fetch()`;
 2. Utilize seus conhecimentos na manipulação do DOM para criar a imagem e ativar o evento de clique do botão!
 
 ## Demo
 
 ![catAPI](./api-cats.gif)
+
 # Créditos
 
-[Stebsnusch](https://github.com/LayanOliveira/basecamp-javascript/tree/main/debug-error)
+[Stebsnusch](https://github.com/stebsnusch/basecamp-javascript/tree/main/javascript-assincrono)
---
+++ Curso-HTML5-CSS3-JS/10-JavaScript-Assíncrono/projeto-html-dogs/assets/js/scripts.js
@@ -0,0 +1,44 @@
+// Solução 1
+const BASE_URL = 'https://dog.ceo/api/breeds/image/random';
+const dogBtn = document.getElementById('change-dog');
+
+const getDogs = async () => {
+    try {
+        const data = await fetch(BASE_URL);
+        const json = await data.json();
+        return json.message;
+    } catch (e) {
+        console.log(e.message);
+    }
+};
+
+const loadImg = async () => {
+    const dogImg = document.getElementById('dog');
+    dogImg.src = await getDogs();
+};
+
+dogBtn.addEventListener('click', loadImg);
+
+loadImg();
+
+console.log(getDogs())
+
+// // Solução 2
+// const BASE_URL = 'https://thatcopy.pw/catapi/rest/'
+// const catBtn = document.getElementById('change-cat')
+
+// const getCats = async () => {
+//     const data = await fetch(BASE_URL)
+//         .then(res => res.json())
+//         .catch(e => console.log(e))
+//     return data.webpurl
+// }
+
+// const loadImg = async () => {
+//     const catImg = document.getElementById('cat')
+//     catImg.src = await getCats()
+// }
+
+// catBtn.addEventListener('click', loadImg)
+
+// loadImg()
