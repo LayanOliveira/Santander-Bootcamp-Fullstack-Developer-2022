3dd2e4377c263e03f26ae0bc8c57dcf60e829b54
LayanOliveira <layan3210@gmail.com>
2022-06-29 02:50:37 -0300

    Adicionado as novas aulas do projeto TypeScript

---
+++ Curso-HTML5-CSS3-JS/15-TypeScript-Project/ex04/any.js
@@ -0,0 +1,15 @@
+"use strict";
+// Porque não devemos usar "any"
+let valorAny;
+valorAny = 3;
+valorAny = 'ola';
+valorAny = true;
+let valorString = 'teste';
+valorString = valorAny;
+let valorString2 = 'testao';
+valorString2 = valorAny;
+function somarString(string1, string2) {
+    console.log(string1 + string2); // any somando booleano (true + true = 2)
+}
+somarString(valorString, valorString2);
+somarString('ola ', ', como vai?');
---
+++ Curso-HTML5-CSS3-JS/15-TypeScript-Project/ex04/any.ts
@@ -0,0 +1,17 @@
+// Porque não devemos usar "any"
+let valorAny: any;
+valorAny = 3;
+valorAny = 'ola';
+valorAny = true;
+
+let valorString: string = 'teste';
+valorString = valorAny;
+let valorString2: string = 'testao';
+valorString2 = valorAny;
+
+function somarString(string1: string, string2: string) {
+    console.log(string1 + string2); // any somando booleano (true + true = 2)
+}
+
+somarString(valorString, valorString2);
+somarString('ola ', ', como vai?');
