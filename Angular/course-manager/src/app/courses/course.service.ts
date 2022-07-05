52930e838596edc9414db5e22c624061b7f9be16
LayanOliveira <layan3210@gmail.com>
2022-07-04 21:05:41 -0300

    Added a course service and a search bar with a filtering function

---
+++ Angular/course-manager/src/app/courses/course.service.ts
@@ -0,0 +1,71 @@
+import { Injectable } from "@angular/core";
+import { Course } from "./course";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class CourseService {
+
+    retrieveAll(): Course[] {
+        return COURSES;
+    }
+
+}
+
+var COURSES: Course[] = [
+    {
+        id: 1,
+        name: 'Angular: CLI',
+        releaseDate: 'July, 04, 2022',
+        description: 'Neste curso, os alunos irão obter um grande conhecimento nos principais recursos do CLI',
+        duration: 120,
+        code: 'XLF-1212',
+        rating: 3,
+        price: 12.99,
+        imageUrl: '/assets/images/cli.png',
+    },
+    {
+        id: 2,
+        name: 'Angular: Forms',
+        releaseDate: 'July, 04, 2022',
+        description: 'Neste curso, os alunos irão obter um grande conhecimento nos principais recursos do CLI',
+        duration: 80,
+        code: 'DWQ-3412',
+        rating: 3.5,
+        price: 24.99,
+        imageUrl: '/assets/images/forms.png',
+    },
+    {
+        id: 3,
+        name: 'Angular: HTTP',
+        releaseDate: 'July, 08, 2022',
+        description: 'Neste curso, os alunos irão obter um grande conhecimento nos principais recursos do CLI',
+        duration: 80,
+        code: 'QPL-8913',
+        rating: 4,
+        price: 36.99,
+        imageUrl: '/assets/images/http.png',
+    },
+    {
+        id: 4,
+        name: 'Angular: Router',
+        releaseDate: 'July, 11, 2022',
+        description: 'Neste curso, os alunos irão obter um grande conhecimento nos principais recursos do CLI',
+        duration: 80,
+        code: 'QHP-1095',
+        rating: 4.5,
+        price: 46.99,
+        imageUrl: '/assets/images/router.png',
+    },
+    {
+        id: 5,
+        name: 'Angular: Animations',
+        releaseDate: 'July, 18, 2022',
+        description: 'Neste curso, os alunos irão obter um grande conhecimento nos principais recursos do CLI',
+        duration: 80,
+        code: 'PWY-9381',
+        rating: 5,
+        price: 56.99,
+        imageUrl: '/assets/images/animations.png',
+    }
+];
