e5aedc768e81fabb26480a287c93662e29ab746b
LayanOliveira <layan3210@gmail.com>
2022-07-19 21:28:35 -0300

    division has no float

--- Java/IDEs-Java/Eclipse-IDE/first-java-program/src/testing/calculator/Calculator.java
+++ Java/IDEs-Java/Eclipse-IDE/first-java-program/src/testing/calculator/Calculator.java
@@ -5,24 +5,24 @@ import java.util.Scanner;
 public class Calculator {
 
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		
-		int a, b;
-		
-		System.out.println("Type the first value: ");
-		a = scan.nextInt();
-		System.out.println("Type the second value: ");
-		b = scan.nextInt();
-		
-		int sum = sum(a,b);
-		int subtract = subtract(a,b);
-		int division = division(a,b);
-		int multiply = multiply(a,b);
-		
-		System.out.println("The sum is: " + sum);
-		System.out.println("The subtraction is: " + subtract);
-		System.out.println("The division is: " + division);
-		System.out.println("The multiplication is: " + multiply);
+		try (Scanner scan = new Scanner(System.in)) {
+			int a, b;
+			
+			System.out.println("Type the first value: ");
+			a = scan.nextInt();
+			System.out.println("Type the second value: ");
+			b = scan.nextInt();
+			
+			int sum = sum(a,b);
+			int subtract = subtract(a,b);
+			int division = division(a,b);
+			int multiply = multiply(a,b);
+			
+			System.out.println("Sum: " + sum);
+			System.out.println("Subtraction: " + subtract);
+			System.out.println("Division: " + division);
+			System.out.println("Multiplication: " + multiply);
+		}
 	}
 
 
